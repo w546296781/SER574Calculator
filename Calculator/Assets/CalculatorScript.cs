03a5b79bde42fdf622496b25f88961fd65b87ce2
Xinkai Wang <546296781@qq.com>
2020-02-20 23:23:59 -0700

    Finish

---
+++ Calculator/Assets/CalculatorScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CalculatorDll;
+
+public class CalculatorScript : MonoBehaviour
+{
+    public InputField input1;
+    public InputField input2;
+    public InputField result;
+    private CalculatorClass cc = new CalculatorClass();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Add()
+    {
+        result.text = cc.Add(System.Convert.ToDouble(input1.text), System.Convert.ToDouble(input2.text)).ToString();
+    }
+
+    public void Sub()
+    {
+        result.text = cc.Substract(System.Convert.ToDouble(input1.text), System.Convert.ToDouble(input2.text)).ToString();
+    }
+
+    public void Mul()
+    {
+        result.text = cc.multiply(System.Convert.ToDouble(input1.text), System.Convert.ToDouble(input2.text)).ToString();
+    }
+
+    public void Div()
+    {
+        result.text = cc.divide(System.Convert.ToDouble(input1.text), System.Convert.ToDouble(input2.text)).ToString();
+    }
+}
---
+++ Calculator/Assets/TestCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CalculatorDll;
+
+public class TestCalculator : MonoBehaviour
+{
+    public double addA;
+    public double addB;
+    public double addResult;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CalculatorClass testdll = new CalculatorClass();
+        Debug.Log(testdll.divide(3, 7));
+        addResult = testdll.Add(addA, addB);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
